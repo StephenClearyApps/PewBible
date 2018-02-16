daea4951a689d3d3ba3a4694583392e643915633
Stephen Cleary <StephenCleary@users.noreply.github.com>
2018-02-15 21:14:33 -0500

    Add Text project.

---
+++ Text/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PewBible.Text
+{
+    public sealed class Book
+    {
+        public string Name { get; set; }
+
+        public int Index { get; set; }
+
+        public List<Chapter> Chapters { get; set; }
+
+        public int BeginVerse { get; set; }
+
+        public int EndVerse { get; set; }
+    }
+}
---
+++ Text/Chapter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PewBible.Text
+{
+    public sealed class Chapter
+    {
+        public int Index { get; set; }
+
+        public int BeginVerse { get; set; }
+
+        public int EndVerse { get; set; }
+    }
+}
