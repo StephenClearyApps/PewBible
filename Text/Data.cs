1979a54658cebd4072a1cd9dd4f9f23b3578dbbe
Stephen Cleary <StephenCleary@users.noreply.github.com>
2018-02-15 21:32:03 -0500

    Move embedded data to Text project.

---
+++ Text/Data.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace PewBible.Text
+{
+    public static class Data
+    {
+        private static readonly Lazy<Stream> _verses = new Lazy<Stream>(() => Assembly.GetExecutingAssembly().GetManifestResourceStream("PewBible.Text.Embedded.verses.dat"));
+
+        public static Stream Verses => _verses.Value;
+    }
+}
