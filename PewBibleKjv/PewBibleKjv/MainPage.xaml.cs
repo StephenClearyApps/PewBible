357b45fdbced609d56a3250edf9ffb1fd77ebc70
Stephen Cleary <StephenCleary@users.noreply.github.com>
2018-02-16 20:37:24 -0500

    Add PewBibleKjv as a shared project to enable embedding native RecyclerView.

---
+++ PewBibleKjv/PewBibleKjv/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.Support.V7.Widget;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms.Xaml;
+
+namespace PewBibleKjv
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
