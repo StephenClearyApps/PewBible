2a0e4cea99e2db897b96b73ddd8a248fb9193fa2
Stephen Cleary <StephenCleary@users.noreply.github.com>
2018-02-16 21:23:29 -0500

    RecyclerView proof of concept.

--- PewBibleKjv/PewBibleKjv/MainPage.xaml.cs
+++ PewBibleKjv/PewBibleKjv/MainPage.xaml.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Android.Support.V7.Widget;
+using Android.Views;
+using Android.Widget;
+using PewBibleKjv.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
 using Xamarin.Forms.Xaml;
@@ -15,6 +18,50 @@ namespace PewBibleKjv
 		public MainPage()
 		{
 			InitializeComponent();
-		}
-	}
+		    var recyclerView = GetNative<RecyclerView>(RecyclerViewParent);
+            recyclerView.SetLayoutManager(new LinearLayoutManager(Android.App.Application.Context));
+            recyclerView.SetAdapter(new TestAdapter(new TestData()));
+            recyclerView.ScrollToPosition(1000);
+        }
+
+	    private static T GetNative<T>(ContentView contentView) where T : Android.Views.View
+	    {
+	        var wrapper = (NativeViewWrapper)contentView.Content;
+	        return (T)wrapper.NativeView;
+	    }
+
+	    public class TestViewHolder : RecyclerView.ViewHolder
+	    {
+	        public TextView View { get; }
+
+	        public TestViewHolder(TextView view) : base(view)
+	        {
+	            View = view;
+	        }
+	    }
+
+	    public class TestAdapter : RecyclerView.Adapter
+	    {
+	        private readonly TestData _data;
+
+	        public TestAdapter(TestData data)
+	        {
+	            _data = data;
+	        }
+
+	        public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+	        {
+	            var vh = (TestViewHolder)holder;
+	            vh.View.Text = _data[position].ToString();
+	        }
+
+	        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+	        {
+                var view = new TextView(parent.Context);
+                return new TestViewHolder(view);
+	        }
+
+	        public override int ItemCount => _data.Count;
+	    }
+    }
 }
