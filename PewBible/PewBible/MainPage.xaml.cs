fefe0e3f3724eab3b444fd25e254f955b01fb07b
Stephen Cleary <StephenCleary@users.noreply.github.com>
2018-02-16 19:26:31 -0500

    Validation that ListView isn't virtualized.

--- PewBible/PewBible/MainPage.xaml.cs
+++ PewBible/PewBible/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PewBible.ViewModels;
 using Xamarin.Forms;
 
 namespace PewBible
@@ -12,6 +13,8 @@ namespace PewBible
 		public MainPage()
 		{
 			InitializeComponent();
+		    ListView1.ItemsSource = new TestData();
+            ListView1.ScrollTo(1000, ScrollToPosition.MakeVisible, animated: false);
 		}
 	}
 }
