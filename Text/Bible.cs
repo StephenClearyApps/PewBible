54de1aace6b8295b6a083a7f35616f2cb1e41683
Stephen Cleary <StephenCleary@users.noreply.github.com>
2018-02-22 11:16:49 -0500

    Start adding Bible text logic (with some basic unit tests).

---
+++ Text/Bible.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PewBible.Text
+{
+    public static class Bible
+    {
+        public static IEnumerable<string> VerseWords(int verseNumber)
+        {
+            var beginEndBytes = new byte[sizeof(int) * 2];
+            Data.VerseIndex.Position = verseNumber * sizeof(int);
+            Data.VerseIndex.Read(beginEndBytes, 0, sizeof(int) * 2);
+            var begin = BitConverter.ToInt32(beginEndBytes, 0);
+            var end = BitConverter.ToInt32(beginEndBytes, sizeof(int));
+
+            var verseDataBytes = new byte[sizeof(ushort) * (end - begin)];
+            Data.Verses.Position = sizeof(ushort) * begin;
+            Data.Verses.Read(verseDataBytes, 0, verseDataBytes.Length);
+            var verseData = new ushort[end - begin];
+            Buffer.BlockCopy(verseDataBytes, 0, verseData, 0, verseDataBytes.Length);
+
+            foreach (var value in verseData)
+            {
+                // Look up the word in either the words or punctuation tables, and adjust casing if necessary.
+                var wordIndex = value & 0x3FFF;
+                var isPunctuation = wordIndex >= Constants.Words.Length;
+                if (isPunctuation)
+                {
+                    yield return Constants.Punctuation[wordIndex - Constants.Words.Length];
+                }
+                else
+                {
+                    var word = Constants.Words[wordIndex];
+                    var wordFlags = value >> 14;
+                    if (wordFlags == 1)
+                        word = word.ToUpperInvariant();
+                    else if (wordFlags == 2)
+                        word = char.ToUpperInvariant(word[0]) + word.Substring(1);
+                    else if (wordFlags == 3)
+                        word = char.ToUpperInvariant(word[0]) + char.ToUpperInvariant(word[1]) + word.Substring(2);
+                    yield return word;
+                }
+            }
+        }
+    }
+}
---
+++ UnitTests/TextUnitTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PewBible.Text;
+using Xunit;
+
+namespace UnitTests
+{
+    public class TextUnitTests
+    {
+        [Fact]
+        public void Gen1_1_Words()
+        {
+            var verse = Bible.VerseWords(0).ToArray();
+            Assert.Equal(new[] { "In", "the", "beginning", "God", "created", "the", "heaven", "and", "the", "earth", "." }, verse);
+        }
+
+        [Fact]
+        public void Rev22_21_Words()
+        {
+            var verse = Bible.VerseWords(31101).ToArray();
+            Assert.Equal(new[] { "The", "grace", "of", "our", "Lord", "Jesus", "Christ", "[", "be", "]", "with", "you", "all", ".", "Amen", "." }, verse);
+        }
+    }
+}
