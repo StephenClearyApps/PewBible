e66fb6df92eaf465b740d6fc50a2ba860877552a
Stephen Cleary <StephenCleary@users.noreply.github.com>
2018-02-26 12:48:17 -0500

    Add FastStartupTest, which shows Xamarin.Forms adds an unacceptable ~4 seconds to startup time just on its own.

---
+++ FastStartupTest/MainActivity.cs
@@ -0,0 +1,61 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.Widget;
+using Android.Views;
+using PewBibleKjv.ViewModels;
+
+namespace FastStartupTest
+{
+    [Activity(Label = "FastStartupTest", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var recyclerView = FindViewById<RecyclerView>(Resource.Id.recyclerView);
+            recyclerView.SetLayoutManager(new LinearLayoutManager(this));
+            recyclerView.SetAdapter(new TestAdapter(new TestData()));
+            recyclerView.ScrollToPosition(1000);
+        }
+        public class TestViewHolder : RecyclerView.ViewHolder
+        {
+            public TextView View { get; }
+
+            public TestViewHolder(TextView view) : base(view)
+            {
+                View = view;
+            }
+        }
+
+        public class TestAdapter : RecyclerView.Adapter
+        {
+            private readonly TestData _data;
+
+            public TestAdapter(TestData data)
+            {
+                _data = data;
+            }
+
+            public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+            {
+                var vh = (TestViewHolder)holder;
+                vh.View.Text = _data[position];
+            }
+
+            public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+            {
+                return new TestViewHolder(new TextView(parent.Context));
+            }
+
+            public override int ItemCount => _data.Count;
+        }
+    }
+}
+
---
+++ FastStartupTest/TestData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace PewBibleKjv.ViewModels
+{
+    public sealed class TestData: IReadOnlyList<string>
+    {
+        public IEnumerator<string> GetEnumerator()
+        {
+            for (var i = 0; i != Count; ++i)
+            {
+                Debug.WriteLine("Enumerating " + i);
+                yield return this[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public int Count => 31102;
+
+        public string this[int index]
+        {
+            get
+            {
+                Debug.WriteLine("Retrieving " + index);
+                return PewBible.Text.Bible.FormattedVerse(index).Text;
+            }
+        }
+    }
+}
