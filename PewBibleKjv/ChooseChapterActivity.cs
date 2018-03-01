540dcaeb87243881e576cd68cdbd70a1af436983
Stephen Cleary <StephenCleary@users.noreply.github.com>
2018-02-28 23:13:53 -0500

    Fill out ChooseChapter buttons.

--- PewBibleKjv/ChooseChapterActivity.cs
+++ PewBibleKjv/ChooseChapterActivity.cs
@@ -1,15 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 
 using Android.App;
 using Android.Content;
+using Android.Database;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using PewBibleKjv.Text;
+using Object = Java.Lang.Object;
 
 namespace PewBibleKjv
 {
@@ -31,6 +34,27 @@ namespace PewBibleKjv
 
             var heading = FindViewById<TextView>(Resource.Id.chooseChapterHeading);
             heading.Text = Structure.Books[bookIndex].Name;
+
+            var grid = FindViewById<GridView>(Resource.Id.chooseChapterGrid);
+            grid.SetColumnWidth(MaximumButtonWidth());
+            grid.Adapter = new ArrayAdapter<int>(this, Resource.Layout.ChooseChapterButton, Structure.Books[bookIndex].Chapters.Select((_, i) => i + 1).ToArray());
+            grid.ItemClick += (_, args) => { Debugger.Break(); };
+        }
+
+        private int MaximumButtonWidth()
+        {
+            var buffer = new FrameLayout(this);
+            var view = LayoutInflater.Inflate(Resource.Layout.ChooseChapterButton, buffer, false);
+            var button = view.FindViewById<Button>(Resource.Id.chooseChapterButton);
+            button.Text = (Structure.Books.Max(x => x.Chapters.Length) + 1).ToString();
+            buffer.AddView(view, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WrapContent, ViewGroup.LayoutParams.WrapContent));
+
+            view.ForceLayout();
+            view.Measure(1000, 1000);
+            var width = view.MeasuredWidth;
+
+            buffer.RemoveAllViews();
+            return width;
         }
     }
 }
