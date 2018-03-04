8d79896c6b017e28a4897e0095f43442d3ff1fd4
Stephen Cleary <StephenCleary@users.noreply.github.com>
2018-03-04 10:03:08 -0800

    Introduce AppFactory for testing.

--- UnitTests/AppUnitTests.cs
+++ UnitTests/AppUnitTests.cs
@@ -8,6 +8,7 @@ using PewBibleKjv.Logic;
 using PewBibleKjv.Logic.Adapters.Services;
 using PewBibleKjv.Logic.Adapters.UI;
 using PewBibleKjv.Text;
+using UnitTests.Util;
 using Xunit;
 
 namespace UnitTests
@@ -17,20 +18,18 @@ namespace UnitTests
         [Fact]
         public void InitialStartup_StartsAtJohn_1_1()
         {
-            var verseView = new Mock<IVerseView>();
-            var app = new App(Mock.Of<IChapterHeading>(), verseView.Object,
-                Mock.Of<ISimpleStorage>(), Mock.Of<IHistoryControls>(), Bible.InvalidAbsoluteVerseNumber);
-            verseView.Verify(x => x.Jump(26045));
+            var verseView = new StubVerseView();
+            var app = AppFactory.Create(verseView: verseView);
+            Assert.Equal(26045, verseView.CurrentAbsoluteVerseNumber);
         }
 
         [Fact]
         public void InitialStartup_HistoryButtonsDisabled()
         {
-            var historyControls = new Mock<IHistoryControls>();
-            var app = new App(Mock.Of<IChapterHeading>(), Mock.Of<IVerseView>(),
-                Mock.Of<ISimpleStorage>(), historyControls.Object, Bible.InvalidAbsoluteVerseNumber);
-            historyControls.VerifySet(x => x.BackEnabled = false);
-            historyControls.VerifySet(x => x.ForwardEnabled = false);
+            var historyControls = new StubHistoryControls();
+            var app = AppFactory.Create(historyControls: historyControls);
+            Assert.False(historyControls.BackEnabled);
+            Assert.False(historyControls.ForwardEnabled);
         }
     }
 }
