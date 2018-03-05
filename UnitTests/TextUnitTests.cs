8826c07ae43011a888ca708bf784d2bd1d324c13
Stephen Cleary <StephenCleary@users.noreply.github.com>
2018-03-05 14:38:57 -0800

    Unit test for span-related bug.

--- UnitTests/TextUnitTests.cs
+++ UnitTests/TextUnitTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PewBibleKjv.Logic;
 using PewBibleKjv.Text;
 using UnitTests.Util;
 using Xunit;
@@ -21,7 +22,7 @@ namespace UnitTests
         [Fact]
         public void Rev22_21_Words()
         {
-            var verse = Bible.VerseWords(31101).ToArray();
+            var verse = Bible.VerseWords(VerseHelper.Find("Revelation", 22, 21).AbsoluteVerseNumber).ToArray();
             Assert.Equal(new[] { "The", "grace", "of", "our", "Lord", "Jesus", "Christ", "[", "be", "]", "with", "you", "all", ".", "Amen", "." }, verse);
         }
 
@@ -36,7 +37,7 @@ namespace UnitTests
         [Fact]
         public void Rev22_21_Formatted()
         {
-            var verse = Bible.FormattedVerse(31101);
+            var verse = Bible.FormattedVerse(VerseHelper.Find("Revelation", 22, 21).AbsoluteVerseNumber);
             Assert.Equal(" The grace of our Lord Jesus Christ be with you all. Amen.", verse.Text);
             Assert.Equal(new []
             {
@@ -47,6 +48,23 @@ namespace UnitTests
                     End = 38,
                 }
             }, verse.Spans, Comparers.SpanComparer);
+        }
+
+        [Fact]
+        public void AllVerses_PassBasicChecks()
+        {
+            for (var i = 0; i != VerseHelper.Find("Revelation", 22).Chapter.EndVerse; ++i)
+            {
+                var verse = Bible.FormattedVerse(i);
+                Assert.False(string.IsNullOrWhiteSpace(verse.Text));
+                foreach (var span in verse.Spans)
+                {
+                    Assert.True(span.Begin >= 0, $"Invalid span begin for {i}");
+                    Assert.True(span.End <= verse.Text.Length, $"Invalid span end for {i}");
+                    Assert.True(span.Begin < span.End, $"Invalid span for {i}");
+                    Assert.False(string.IsNullOrWhiteSpace(verse.Text.Substring(span.Begin, span.End - span.Begin)), $"Invalid span text for {i}");
+                }
+            }
         }
     }
 }
