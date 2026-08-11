288d6ac4da8ec9c1f8a37b03a4120d25b0c97ce2
Stephen Cleary <900597+StephenCleary@users.noreply.github.com>
2026-08-11 18:49:34 -0400

    Minor code updates.

--- UnitTests/Util/StubSimpleStorage.cs
+++ UnitTests/Util/StubSimpleStorage.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using PewBibleKjv.Logic.Adapters.Services;
 
 namespace UnitTests.Util
@@ -11,7 +7,7 @@ namespace UnitTests.Util
     [ExcludeFromCodeCoverage]
     public sealed class StubSimpleStorage: ISimpleStorage
     {
-        private readonly Dictionary<string, string> _data = new Dictionary<string, string>();
+        private readonly Dictionary<string, string> _data = [];
 
         public string Load(string key)
         {
